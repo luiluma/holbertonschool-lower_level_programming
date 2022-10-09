658b4a5e2f4d8127e0c1a8e6c90228b912f0777c
luiluma <luma24lopez@gmail.com>
2022-10-08 21:44:53 -0700

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,9 +13,9 @@ int main(void)
 
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 
-printf("Size of an int: %d byte(s)\n", sizeof(init));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
 
-printf("Size of a long int: %d byte(s)\n", sizeof(long init));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 
