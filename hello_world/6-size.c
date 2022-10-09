79060c58c5a5a379cb01ffec5705be0accd8f1b2
luiluma <luma24lopez@gmail.com>
2022-10-08 21:47:58 -0700

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- *main - Punto de entrada
+ *main - Entry point
  *
- *Return: Always 0
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
-
 
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 
@@ -21,5 +20,6 @@ printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 
 printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-return o;
+return (0);
+
 }
