17f81cc2057e5f5e0582f7bb2e7f2c082a530159
luiluma <luma24lopez@gmail.com>
2022-10-08 19:53:45 -0700

    task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
- 
  */
 
 int main(void)
@@ -14,5 +12,4 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
-
 }
