3ac5abcc7adfdcdbcdb8d31fcc7f4889219e6b46
luiluma <luma24lopez@gmail.com>
2022-10-08 18:30:04 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Descripcion: punto "Programming is like building a multilingual puzzle"
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Suceso)
+ *
+ *       */
 int main(void)
 
 {
