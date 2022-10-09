80e9e278ba73ba9eac3b45cdfd190b1cb2a8892a
luiluma <luma24lopez@gmail.com>
2022-10-09 01:52:14 -0700

    task adv

---
+++ hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 1
+ */
+int main(void)
+
+{
+
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, str1);
+
+return (1);
+
+}
