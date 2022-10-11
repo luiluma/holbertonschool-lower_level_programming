d602476fe017be867e9e80480fb5bd7321d2fe2a
luiluma <luma24lopez@gmail.com>
2022-10-11 00:05:27 -0700

    task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,22 +7,15 @@
 
 int main(void)
 {
-
 int n;
-
 for (n = 0; n <= 9; n++)
-
 {
-
 putchar((n % 10) + '0');
-
 if (n == 9)
-continue;			
+continue;
 putchar(',');
 putchar(' ');
-
 }
-
 putchar('\n');
 return (0);
 }
