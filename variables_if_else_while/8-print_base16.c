bf028e0e746a63f336adcc24bdf598b89ba82c55
luiluma <luma24lopez@gmail.com>
2022-10-10 23:47:40 -0700

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - the numbers of base 16 in lowercase
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+putchar('0' + n);
+
+if (n == 9)
+
+{
+putchar('\n');
+
+break;
+}
+else
+{
+
+putchar('n');
+n++;
+}
+}
+
+return (0);
+}
