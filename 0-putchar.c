cfb6453b36e852a6f1c2a591181240ceba658e0c
luiluma <luma24lopez@gmail.com>
2022-10-11 09:08:39 -0700

    task 0

---
+++ 0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints putchar
+ *
+ *Return: 0
+ */
+int main(void)
+
+_putchar ('\n');
+return (0);
---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - prints _putchar
+ *
+ *Return: 0
+ */
+
+int main(void)
+
+_putchar ('\n');
+return (0);
