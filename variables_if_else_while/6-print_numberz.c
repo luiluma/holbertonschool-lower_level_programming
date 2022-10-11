75366a3279b76422d5d8c20ca1d6a31e5662b8d0
luiluma <luma24lopez@gmail.com>
2022-10-10 21:05:16 -0700

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - all single digit numbers of base 10 starting from 0
+ *
+ *Return: 0
+ */
+int main(void)
+{
+int a = 0;
+while (a < 10)
+{
+putchar ('0' + a);
+a++
+}
+putchar ('\n');
+return (0);
+}
