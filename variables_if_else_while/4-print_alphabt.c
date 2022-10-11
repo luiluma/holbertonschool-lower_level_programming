b4f8de606730b8256ec37c755911539c09d4e7bd
luiluma <luma24lopez@gmail.com>
2022-10-10 19:22:41 -0700

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - the alphabet in lowercase
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+char a = 'a';
+{
+
+while (a = 'z')
+if (a == 'e' || a == 'q')
+{
+a++;
+}
+else
+{
+putchar (a);
+a++;
+}
+
+{
+putchar ('\n');
+a++;
+return (0);
+}
