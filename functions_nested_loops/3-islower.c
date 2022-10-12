c1d8c7532a195aec42b08584da8d74bdb0d4e7a4
luiluma <luma24lopez@gmail.com>
2022-10-12 04:22:48 -0700

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <ctype.h>
 /*
- * islower - verifica si una letra es minuscula
+ * _islower - verifica si una letra es minuscula
  *
  *c: the letter the function checks
  *
  *Return: 1 if c is lowercase, 0 otherwise
  */
 
-int islower(int c);
+int _islower(int 'c');
 
 {
 	char ch;
