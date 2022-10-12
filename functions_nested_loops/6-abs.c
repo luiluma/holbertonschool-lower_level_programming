bda7c2dd30732505f41d4376f17e6794be4a7c3d
luiluma <luma24lopez@gmail.com>
2022-10-12 09:23:21 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+   *_abs - calcula el valor absoluto de un entero
+   *
+   *Description: encontrar el valor absoluto del entero
+   *
+   *@a: the integer
+   *
+   *Retorno: 0
+   */
+int _abs(int a)
+{
+	if (a < 0)
+
+	{
+	a = a -1;
+
+		return (a);
+	}
+	else
+
+		return (a);
+}
+
