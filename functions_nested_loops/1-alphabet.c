9d50cf13b2d65fbb8a187e4375c6aaa6ceed9a4d
luiluma <luma24lopez@gmail.com>
2022-10-11 20:35:28 -0700

    question 2

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -10,17 +10,17 @@ void print_alphabet(void)
 	char i;
 	int n;
 
-	for (n = 0; n < 10; n++)
-	{
+	for (n = 0; n < 10; n++);
+{
 
 		for (i = 'a'; i <= 'z'; i++)	
-		{
+{
 
 			_putchar(i);
 
-		}
+}
 
-	}
+}
 
 
 	_putchar('\n');
