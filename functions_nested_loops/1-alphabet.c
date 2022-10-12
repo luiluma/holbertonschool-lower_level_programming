14d7138e438cf0e897ffcdd444bcde4b3b8464e1
luiluma <luma24lopez@gmail.com>
2022-10-11 19:49:57 -0700

    task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,24 @@
 #include "main.h"
-
-/*
- *print_alphabet - print alphabet
- *
- */
+/**
+   *print_alphabet - prints the alphabet
+   *
+   *Return: 0
+   */
 void print_alphabet(void)
 
 {
+	char n;
 
-char letter = 'a';
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (n = 'a'; n <= 'z'; n++)
+
+{
+
+	_putchar(n);
+
+}
+
 
 	_putchar('\n');
+
 }
