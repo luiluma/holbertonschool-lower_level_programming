9622e52e4b73038c1b49ea969d1f79f18005fbb2
luiluma <luma24lopez@gmail.com>
2022-10-12 20:04:54 -0700

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,18 +9,18 @@ void jack_bauer(void)
 
 	h = 0;
 
-	while (i < 24)
+	while (h < 24)
 	{
 		m = 0;
 		while (m < 60)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
-			j++
+			m++;
 		}
 
 	}
