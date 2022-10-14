e8dca8371cff35d511feb8dbdd87940b94958a23
luiluma <luma24lopez@gmail.com>
2022-10-14 16:48:21 -0700

    fizz,buzz,fizbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+  *main - Prints 1-100 Replaces multiples of 3 Fizz; 5 Buzz, 3 y 5 FizzBuzz
+  *
+  *Return: 0
+  */
+
+int main(void)
+{
+	int i;
+
+	char Fizz, Buzz, FizzBuzz;
+
+	for (i = 1; i <= 100; i++)
+	{
+
+		if (i % 3 == 0 && i % 5 == 0)
+
+			printf("%d", FizzBuzz);
+
+		else if ((i % 3) == 0)
+
+			printf("%d", Fizz);
+
+		else if ((i % 5) == 0)
+		{
+
+			if (i == 100)
+
+			printf("%d", Buzz);
+
+		else
+			printf("%d", Buzz);
+
+		}
+
+		else
+			printf("%i", i);
+
+	}
+
+			putchar('\n');
+
+				return (0);
+}
