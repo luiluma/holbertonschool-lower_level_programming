8a8a7923ff49443e0c3f8f07e9f4679e95a74c08
luiluma <luma24lopez@gmail.com>
2022-10-14 00:32:48 -0700

    diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *print_line - writes a line n times
+  *
+  * @n: the number of lines
+  * Return: 0
+  */
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		putchar('\');
+		i++;
+	}
+	putchar('\n');
+}
