d0618369b2123e7881d15764d7ce68756f2b0f39
luiluma <luma24lopez@gmail.com>
2022-10-13 22:47:16 -0700

    multiplicacion 2 num

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,11 +2,11 @@
 /**
    * mul - multiplies two integers
    * @a: the first integer to multiply
-   * main - funtion principal
+   * 
    * @b: the second integer to multiply
    * Return: the result
    */
-int mul(int a, int b);
+int mul(int a, int b)
 {
 
 	int mul = a * b;
