2b572d6e6fed68950ee2201601796b9f33775630
luiluma <luma24lopez@gmail.com>
2022-10-13 21:38:26 -0700

    task 0

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/* prototype */
+int _isupper(int c);
+#endif /*MAIN_H*/
