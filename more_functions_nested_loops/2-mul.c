960d5266b667693f0a7d554c1c64a3d0a6a2db23
luiluma <luma24lopez@gmail.com>
2022-10-13 22:42:59 -0700

    multiplicacion 2 num

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,16 +1,18 @@
 #include "main.h"
-
 /**
    * mul - multiplies two integers
    * @a: the first integer to multiply
-   * @r: result
+   * main - funtion principal
    * @b: the second integer to multiply
    * Return: the result
    */
-int mul(int a, int b; int r)
+int main(void)
+
 {
+	int a;
+	int b;
 
-	r = a * b;
+	int mul = a * b;
 
-	return (r);
+	return (mul);
 }
