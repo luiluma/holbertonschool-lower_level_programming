7ecd081b7e9c7da1e17a37f8b2b4c57a7db68a8c
luiluma <luma24lopez@gmail.com>
2022-10-14 00:09:13 -0700

    print 10 number from 0-14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * more_numbers - prints 10 times the numbers from 0-14
+  *
+  * Return: 0
+  */
+
+void more_numbers(void)
+{
+
+	int i;
+	int x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 0; x < 15; x++)
+		{
+			if (x > 9)
+
+				_putchar(x / 10 + '0');
+
+			_putchar(x % 10 + '0');
+
+		}
+
+		_putchar('\n');
+	}
+}
+
