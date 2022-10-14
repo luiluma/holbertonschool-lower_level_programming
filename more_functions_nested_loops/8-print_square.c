25ba8ca65802349d2800f3edaa917b1fd248e90a
luiluma <luma24lopez@gmail.com>
2022-10-14 13:58:12 -0700

    square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+  * print_square -  that prints a square, followed by a new line
+  *
+  * @size: 
+  * Return: always 0
+  */
+
+ void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+
+	{
+
+		for (i = 1; i <= size; i++)
+		{
+
+			for (j = 1; j <= size; j++)
+			{
+
+				putchar(35);
+
+				if (j == size)
+				putchar('\n');
+
+			}
+
+		}
+
+	}
+	else
+		putchar('\n');
+
+}	
