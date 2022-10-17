fbc5490c4aa7aefafc46c82b04d9f394bff10d03
luiluma <luma24lopez@gmail.com>
2022-10-17 15:23:54 -0700

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+/**
+  * _strlen - returns the length of a string
+  *
+  * @s: the string  whose length to return
+  *
+  * RETURN: the length of a string
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != 0)
+	{
+		c++;
+	}
+
+	return(c);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,4 +10,5 @@
 /*PROTOTYPE*/
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif /*MAIN_H*/
