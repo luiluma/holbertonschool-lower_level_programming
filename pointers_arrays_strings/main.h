0e4863c65efbaf224b63a1f89cb51578c0fc1058
luiluma <luma24lopez@gmail.com>
2022-10-17 14:41:49 -0700

    reset_to_98

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*LIBRARIES*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <ctype.h>
+/*PROTOTYPE*/
+void reset_to_98(int *n);
+#endif /*MAIN_H*/
