76c96668749d20b6519847637d7045afba9f8968
luiluma <luma24lopez@gmail.com>
2022-10-17 18:04:38 -0700

    print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * _strlen - string counter
+  * @s: string
+  * Return: result
+  */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return(i);
+}
+
+/**
+  * print_rev - reverse string
+  * @s: string
+  */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s) -1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
