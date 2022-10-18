875b12417c5a929ebb86585372ec74f0c28f6d91
luiluma <luma24lopez@gmail.com>
2022-10-18 13:32:13 -0700

    puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -20,6 +20,19 @@ void puts2(char *str)
 		i += 2;
 	}
 	putchar('\n');
+}
+
+#include "main.h"
+/**
+  * _strlen - return length of a string
+  * @s: string
+  * Return: length of the string
+  */
+
+int _strlen(char *s)
+{
+
+	int i = 0;
 
 	while (s[] != '\0')
 		i++;
