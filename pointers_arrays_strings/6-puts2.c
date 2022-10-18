d3e55c4f490678881a4e685c1888491cc2027671
luiluma <luma24lopez@gmail.com>
2022-10-18 13:26:08 -0700

    puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * puts2 - prints one character out of two, starting with the first
+  *
+  * @str: string to look at
+  *
+  * Return: 0
+  */
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = _strlen(str) - 1;
+
+	while (i <= len)
+	{
+		putchar(str[i]);
+		i += 2;
+	}
+	putchar('\n');
+
+	while (s[] != '\0')
+		i++;
+
+	return (i);
+}
