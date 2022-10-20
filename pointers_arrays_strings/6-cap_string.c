acf6c86e089bf28ad0c4a81f798ba9de23096f04
luiluma <luma24lopez@gmail.com>
2022-10-20 10:59:12 -0700

    cap string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,16 +9,23 @@ char *cap_string(char *scr)
 {
 	int x, y, z;
 	char p[] = {44, 59, 33, 32, 46, 63, 34, 40, 41, 123, 125, 10, 9};
-	z = p[3];
 
 	y = strlen(scr);
 	x = 0;
 	while (x < y)
 	{
-		if (scr[x] == z)
+		if (x == 0 && scr[x + 1] >= 'a' && scr[x + 1] <= 'z')
+			scr[x] -= 32;
+
+		for (z = 0; z < 13; z++)
 		{
-			if (scr[x + 1] >= 'a' && scr[x + 1] <= 'z')
-				scr[x + 1] -= 32;
+			if (scr[x] == p[z])
+			{
+				if (scr[x + 1] >= 'a' && scr[x + 1] <= 'z')
+				{
+					scr[x + 1] -= 32;
+				}
+			}
 		}
 		x++;
 	}
