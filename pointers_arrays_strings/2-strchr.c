0cec936794b692f31a04d67985f181eeec21356e
luiluma <luma24lopez@gmail.com>
2022-10-21 09:50:40 -0700

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _strchr - locates character in a string
+  *
+  * @s: string check
+  * @c: character we´re looking
+  *
+  * Return: pointer a the first ocurrance of the character c in the string
+  *
+  */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+		s++;
+
+		return (s);
+
+	}
+	if (c == '\0')
+	return (s);
+}
+
+
+return (NULL);
