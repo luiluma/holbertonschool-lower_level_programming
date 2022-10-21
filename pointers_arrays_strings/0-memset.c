51bea4681fd6d43f126a2c328952df85378e23de
luiluma <luma24lopez@gmail.com>
2022-10-20 19:04:26 -0700

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _memset - fills a memory area with repeated bytes
+  * @s: pointer to the memory area
+  * @n: bytes of the memory area pointed
+  * @b: constant byte
+  * Return: pointer to the memory area @s
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
