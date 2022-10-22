cc31ab45f4bd9b7fdc5807522a5e7ea4ce78ad12
luiluma <luma24lopez@gmail.com>
2022-10-21 19:29:00 -0700

    chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
  /**
-    * print_chessboard - prints the chessboard
+    * print_chessboard -  that prints the chessboard
     * @a: pointer to pieces to print
     *
     * Return: void
     */
 
 void print_chessboard(char (*a)[8])
-
 {
 	int c, f;
 
