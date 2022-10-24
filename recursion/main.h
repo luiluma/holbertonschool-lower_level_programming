fad77c2858743495a012309ea66b85b9edf52e49
luiluma <luma24lopez@gmail.com>
2022-10-24 11:43:57 -0700

    .

---
+++ recursion/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*libraries*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <time.h>
+
+/*prototype*/
+
+void _puts_recursion(char *s);
+
+#endif /*MAIN-H*/
