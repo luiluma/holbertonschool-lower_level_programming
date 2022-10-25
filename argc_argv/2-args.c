22ce51214682d8c51cd0605b6591485465832b91
luiluma <luma24lopez@gmail.com>
2022-10-24 23:42:41 -0700

    .

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+   * main - prints the number of arguments passed into it
+   *
+   * @argc: argument counter
+   * @argv: argument vector
+   * @i: variable
+   * Return: 0
+   */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
