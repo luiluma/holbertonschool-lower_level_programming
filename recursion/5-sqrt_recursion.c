1bb3d11a54a2a4663c074b78acfa64153afb21d8
luiluma <luma24lopez@gmail.com>
2022-10-24 18:02:31 -0700

    sqrt recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _sqrt_recursion - square root of a number
+  * @n: the number
+  *
+  * Return: square root
+  */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0);
+		return (-1);
+}
