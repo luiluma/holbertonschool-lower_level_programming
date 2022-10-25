ae2b2141a0fd8e6c8dcc58852394b6945d2fca70
luiluma <luma24lopez@gmail.com>
2022-10-24 23:52:58 -0700

    1args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+   * main - prints the number of arguments passed into it
+   *
+   * @argc: arg counter
+   * @argv: arg vector
+   *
+   * Return: 0
+   */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	(void)argv;
+
+	for (a = 0; a <= argc; a++)
+	{
+		printf("%d\n", a - 2);
+
+		return (0);
+	}
+}
