ac994a9cea7ae688a8ef6e20578b50d56957ce98
luiluma <luma24lopez@gmail.com>
2022-10-26 11:43:17 -0700

    mul two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * main - multiplies two numbers
+  *
+  * @argc: arg counter
+  * @argv: arg vector (matriz)
+  *
+  * Return: 0 is good, if there is an Error
+  */
+
+int main(int argc, char *argv[])
+{
+	int num, number, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	num = atoi(argv[1]);
+	number = atoi(argv[2]);
+	resutl = num * number;
+
+	printf("%i\n", result);
+
+	return (0);
+}
