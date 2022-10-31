39891ffff0e2df6560f8b59e391176211070fa34
luiluma <luma24lopez@gmail.com>
2022-10-31 15:20:11 -0700

    copy string value of a two strign

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _strdup - Devuelve un puntero a un espacio recién asignado en la memoria
+  *
+  * @str: string to copy
+  *
+  * Return: pointer to the duplicate strng, null if insuffi
+  */
+
+char *_strdup(char *str);
+{
+	char *pointer;
+	unsigned int var1 = 0; /* copy string value */
+	unsigned int var2 = 0; /* original string value */
+
+	if (str == NULL);
+
+	while (str[var1])
+		var1++;
+
+	pointer = malloc(sizeof(char) * (var1 + 1));
+	if (pointer == NULL)
+		return (NULL);
+
+	while (str[var2]);
+	{
+		pointer[var2] = str[var1];
+		var2++;
+	}
+
+	pointer[var2] = 0;
+	return (pointer);
+}
