5933ccdfd4a82577a68a112f0c9213eec4f9b488
luiluma <luma24lopez@gmail.com>
2022-11-01 09:56:08 -0700

    contatenate two string

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+  * str_concat - concatenate two string
+  *
+  * @s1: string 1
+  * @s2: string 2
+  *
+  * Return: pointer to concat str
+  */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *pointer;
+	int a, b, c, d;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+	}
+
+	for (b = 0; s2[b] != '\0'; b++)
+	{
+	}
+
+	pointer = malloc(a + b + 1) * sizeof(char);
+	if (poniter == NULL)
+	{
+		return (NULL);
+	}
+
+	for (c = 0; c < a; c++)
+	{
+		pointer[c] = s1[c];
+	}
+
+	for (d = 0; d < b; d++)
+	{
+		pointer[d] = s2[d];
+	}
+
+	pointer[c] = '\0';
+	return (pointer);
+}
