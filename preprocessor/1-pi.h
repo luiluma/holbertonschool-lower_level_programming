01ef616ab9acf2963662fcb3c49c5eedf75156fb
luiluma <luma24lopez@gmail.com>
2022-11-04 16:25:58 -0700

    suma

--- preprocessor/1-pi.h
+++ preprocessor/1-pi.h
@@ -1,6 +1,4 @@
 #ifndef _1_PI_H_
 #define _PI_
-
 #define PI (3.14159265359)
-
 #endif
---
+++ preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - check code
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	int x;
+
+	x = SUM(x, y)
+	printf("%d\n", x);
+	return (0);
+}
