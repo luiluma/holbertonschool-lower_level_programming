ad3fc0673ece59effdba92243c05cbe13a04e86d
luiluma <luma24lopez@gmail.com>
2022-11-07 15:41:39 -0800

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * struct dog - struct of the dog
+  *
+  * @name: name dog
+  * @age: age dog
+  * @owner: dog owner
+  * Return: 0
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+int main(void)
+{
+	struct dog dog1;
+
+	dog1.name = "zeus";
+	do1.age = 5;
+	dog1.owner = "luisa";
+
+	return (0);
+}
+
