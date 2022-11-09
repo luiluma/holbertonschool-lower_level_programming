d6d736782a0906c24ec5c4e6cd80d2b3268395cf
luiluma <luma24lopez@gmail.com>
2022-11-08 19:00:03 -0800

    printdog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+  * print_dog - prototype struct dog
+  * @d: struct
+  *
+  * Return: 0
+  */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d ->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n" d->name);
+
+
+		printf("Age: %f\n", d->name);
+
+		if (d -> owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
+
