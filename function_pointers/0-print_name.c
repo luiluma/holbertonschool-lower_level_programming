875a8886728c72b3e8d8a738da0fb8574a302a98
luiluma <luma24lopez@gmail.com>
2022-11-09 08:00:48 -0800

    function pointers name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
 
 /**
+  * print_name - name a prints
+  * @name: name
+  * @f: function point
   *
-  *
-  *
-  *
+  * Return: 0
   */
 
 void print_name(char *name, void (*f)(char *))
