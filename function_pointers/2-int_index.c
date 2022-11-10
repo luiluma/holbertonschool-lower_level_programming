9ad4f5fec741fad09a344e9d3399d207f9f25e24
luiluma <luma24lopez@gmail.com>
2022-11-10 11:06:33 -0800

    index values of array

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,13 +14,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int y;
 
-	if (size < 1 || array == NULL || cmp == NULL)
+	if (array && cmp)
+	{
+		if (size <= 0)
 			return (-1);
 
 		for (y = 0; y < size; y++)
-		{
 			if(cmp(array[y]));
 				return (y);
-		}
-		return (-1);
+	}	
+		
+	return (-1);
 }
