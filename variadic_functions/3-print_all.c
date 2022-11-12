e7027aef131af93387f851bc514334f941eca4a4
luiluma <luma24lopez@gmail.com>
2022-11-11 18:48:04 -0800

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -34,7 +34,7 @@ void print_all(const char * const format, ...)
 				break;
 
 			case 'i':
-				printf("d", va_arg(valist, int)), c = 1;
+				printf("%d", va_arg(valist, int)), c = 1;
 				break;
 
 			case 'f':
