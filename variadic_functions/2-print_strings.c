8798c92905dfa58c62444e9d44384c88f711e6da
luiluma <luma24lopez@gmail.com>
2022-11-11 18:32:33 -0800

    print string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", og);
 		else
 			printf("(nil)");
-					
+
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
