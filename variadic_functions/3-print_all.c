684d3a081a851130fa49c49236ea0c8d8b4159d2
luiluma <luma24lopez@gmail.com>
2022-11-11 18:52:19 -0800

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -25,22 +25,18 @@ void print_all(const char * const format, ...)
 				printf(", ");
 				break;
 			} j++;
-
 		}
 		switch (format[i])
 		{
 			case 'c':
 				printf("%c", va_arg(valist, int)), c = 1;
 				break;
-
 			case 'i':
 				printf("%d", va_arg(valist, int)), c = 1;
 				break;
-
 			case 'f':
 				printf("%f", va_arg(valist, double)), c = 1;
 				break;
-
 			case 's':
 				str = va_arg(valist, char *), c = 1;
 				if (!str)
