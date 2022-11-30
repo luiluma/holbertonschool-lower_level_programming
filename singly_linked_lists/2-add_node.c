9ab3c64fc1c3bd1cf66742b6fda2021f87b1868b
luiluma <luma24lopez@gmail.com>
2022-11-30 16:43:42 -0500

    adds a new node at the beginning

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,2 +1,30 @@
 #include "lists.h"
 
+/**
+ * add_node - new node at beginning of list
+ * @head: Start pointer of node
+ * @str: string 
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *point;
+	int i = 0;
+
+	point = malloc(sizeof(list_t));
+
+	if (point == NULL)
+		return (NULL);
+
+	point->str = strdup(str);
+
+	for (; str[i]; i++)
+	{}
+
+	point->len = i;
+	point->next = *head;
+	*head = point;
+
+	return (*head);
+}
