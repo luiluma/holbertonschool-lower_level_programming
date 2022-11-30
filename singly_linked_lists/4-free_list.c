1dcc0aadad406308d80a2a33bfec4874e5cc7fc1
luiluma <luma24lopez@gmail.com>
2022-11-30 15:16:28 -0500

    free list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,20 +1,18 @@
 #include "lists.h"
 /**
  * free_list - free a list
- * 
+ *
  * @head: starting point of the list
- * Return: 0
 */
 
 void free_list(list_t *head)
 {
     list_t *aux;
 
-	if (head != NULL)
+	while (head != NULL)
 	{
 		aux = head->next;
 		free(head->str);
 		free(head);
-		head = aux;
 	}
 }
