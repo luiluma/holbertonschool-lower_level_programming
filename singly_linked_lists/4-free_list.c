655537d7510ef50eb3098a25e922a39db21aa58f
luiluma <luma24lopez@gmail.com>
2022-11-30 15:19:25 -0500

    free list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,13 +7,13 @@
 
 void free_list(list_t *head)
 {
-    list_t *aux;
+	list_t *aux;
 
 	while (head != NULL)
 	{
 		aux = head->next;
 		free(head->str);
 		free(head);
-        head = aux;
+		head = aux;
 	}
 }
