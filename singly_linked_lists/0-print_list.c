1a60f2f1bbaee2d1ff12da34beeadb065d54b486
luiluma <luma24lopez@gmail.com>
2022-11-29 19:10:57 -0500

    print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,5 +13,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
 	}
+	return (count);
 }
