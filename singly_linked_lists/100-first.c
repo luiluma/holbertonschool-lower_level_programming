6073216d770496f6895148de7ecb034e2fac75aa
luiluma <luma24lopez@gmail.com>
2022-11-30 20:57:02 -0500

    avance

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - 
+ * Return: 0
+*/
+
+int main (void)
+{
+    printf("You're beat! and yet, you must allow");
+    printf("\nI bore my house upon my back!\n");
+}
