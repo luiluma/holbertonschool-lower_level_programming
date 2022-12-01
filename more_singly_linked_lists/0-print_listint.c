6b9078f37571b0b9ea1d970020cd62e5735ec1e5
luiluma <luma24lopez@gmail.com>
2022-11-30 22:00:40 -0500

    new projecct

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -2,18 +2,18 @@
 
 /**
  * print_listint - prints all the elements
- * 
+ *
  * @h: point star node
  * Return: 0
- * 
+ *
 */
 
 size_t print_listint(const listint_t *h)
 {
-    int i = 0;
+	int i = 0;
 
-    while (h)
-    {
+	while (h)
+	{
 		printf("%d\n", h->n);
 		h = h->next;
 		i++;
