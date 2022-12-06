67ac90c8ea3c14b3e0cee2e9ccc8685400af07f1
luiluma <luma24lopez@gmail.com>
2022-12-06 18:55:55 -0500

    doubly linked lists

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,16 +1,15 @@
 #include "lists.h"
-
 /**
- * print_listint - prints all the elements
+ * print_dlistint - prints all the elements
  *
  * @h: point star node
  * Return: 0
  *
 */
 
-size_t print_listint(const listint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
-	int i = 0;
+	size_t i = 0;
 
 	while (h)
 	{
