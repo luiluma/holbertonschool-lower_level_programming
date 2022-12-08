5cfde6e594fe9995f13a598434143b1a29613f9a
luiluma <luma24lopez@gmail.com>
2022-12-08 17:26:29 -0500

    file i|o

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * append_text_to_file - read a file.
+ * @filename: name of the file
+ * @text_content: content to the file.
+ * Return: 1 on success.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, i = 0;
+
+	file = open(filename, O_RDWR | O_APPEND);
+
+	if (text_content == NULL)
+		return (1);
+
+	if (file == -1 || filename == NULL)
+		return (-1);
+
+	for (; text_content[i]; i++)
+	{}
+	write(file, text_content, i);
+	close(file);
+	return (1);
+}
